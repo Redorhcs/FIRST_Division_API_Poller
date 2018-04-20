716cba26d9b8b57e359fe49cc3ec9b78a41f676f
RS Tyler Schroder <11656073+Redorhcs@users.noreply.github.com>
2018-04-20 14:17:45 -0400

    Update API_Poller.ps1
    Updated to include more details

--- API_Poller.ps1
+++ API_Poller.ps1
@@ -1,10 +1,23 @@
+$starttime = get-Date -format r
+"Schroder's Detroit Division API Script"
+"---------------------------------------"
+"Located at https://github.com/Redorhcs/FIRST_Division_API_Poller
+License GNU GPL 3 @ https://github.com/Redorhcs/FIRST_Division_API_Poller/blob/master/LICENSE"
+"Run started at:" 
+$starttime
+start-sleep -Seconds 2
 While(1)
 {
 clear
+"Schroder's Detroit Division API Script"
+"---------------------------------------"
 $request = 'http://frc.divisions.co/api/v2/status'
 Invoke-WebRequest $request |
 ConvertFrom-Json |
 select -expand north
 get-Date -format r
 start-sleep -seconds 60
+"---------------------------------------"
+"Located at https://github.com/Redorhcs/FIRST_Division_API_Poller
+License GNU GPL 3 @ https://github.com/Redorhcs/FIRST_Division_API_Poller/blob/master/LICENSE"
 }
